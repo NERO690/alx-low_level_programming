142cd5afc16bf14bc9cabcf6f05a983fc33a188d
NERO690 <nelsonchristmas35@gmail.com>
2023-10-13 21:17:20 -0700

    lover

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
-  * add_dnodeint_end - adds a new node at the end
- * of a dlistint_t list
+ * add_dnodeint_end - add a new node at end of a dlistint_t list
+ * @head: head of list
  * @n: value of new node
  * Return: the address of new element, or NULL if it failed
  **/
@@ -27,4 +27,4 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	*head = new_node;
 
 	return (new_node);
-}}
+}
